0506f9f6c0360c964365d96fc796656edd52d1fd
labib farag <labib.farag@ibm.com>
2018-10-14 13:25:02 +0200

    update code for sample chain code

--- fabric-samples/chaincode/SimpleChaincode/startFabric.sh
+++ fabric-samples/chaincode/SimpleChaincode/startFabric.sh
@@ -11,31 +11,31 @@ set -e
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 LANGUAGE=${1:-"golang"}
-CC_SRC_PATH=github.com/chaincode_example02/go
+CC_SRC_PATH=github.com/SimpleChaincode/go
 if [ "$LANGUAGE" = "node" -o "$LANGUAGE" = "NODE" ]; then
-	CC_SRC_PATH=/opt/gopath/src/github.com/chaincode_example02/node
+	CC_SRC_PATH=/opt/gopath/src/github.com/SimpleChaincode/node
 fi
 
 if [ "$LANGUAGE" = "java" -o "$LANGUAGE" = "JAVA" ]; then
-	CC_SRC_PATH=/opt/gopath/src/github.com/chaincode_example02/java
+	CC_SRC_PATH=/opt/gopath/src/github.com/SimpleChaincode/java
 fi
 # clean the keystore
 rm -rf ./hfc-key-store
 
 # launch network; create channel and join peer to channel
-cd ../basic-network
+cd ../../basic-network
 ./start.sh
 
 # Now launch the CLI container in order to install, instantiate chaincode
 # and prime the ledger with our 10 cars
 docker-compose -f ./docker-compose.yml up -d cli
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n chaincode_example02 -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n chaincode_example02 -l "$LANGUAGE" -v 1.0 -c '{"Args":["init","a","10","b","20"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n SimpleChaincode -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n SimpleChaincode -l "$LANGUAGE" -v 1.0 -c '{"Args":["init","a","10","b","20"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode query -o orderer.example.com:7050 -C mychannel -n chaincode_example02 -c '{"function":"query","Args":["a"]}'
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode query -o orderer.example.com:7050 -C mychannel -n SimpleChaincode -c '{"function":"query","Args":["a"]}'
 
 printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
-printf "Start by installing required packages run 'npm install'\n"
+
 
 
